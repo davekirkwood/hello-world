51e975934ef2add6d2dff7d21b03d780a86196e5
dave <dave@dave-Aspire-5750>
2015-03-22 09:47:08 +0900

    Commit to local

---
+++ GitTest/src/Main.java
@@ -0,0 +1,14 @@
+/******************************************************
+ *        BRaVE Copyright 2015 David Kirkwood
+ ******************************************************
+ * University of Birmingham Railway Virtual Environment
+ *******************************************************
+ */
+
+public class Main {
+
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+   }
+   
+}
