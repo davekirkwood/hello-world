dd628ccd23b0a459f1597abd4327f8e6ad7dbbfe
dave <dave@dave-Aspire-5750>
2015-03-29 02:08:44 +0100

    added class

---
+++ GitTest/src/mypackage/NewMyPackageClass.java
@@ -0,0 +1,11 @@
+/******************************************************
+ *        BRaVE Copyright 2015 David Kirkwood
+ ******************************************************
+ * University of Birmingham Railway Virtual Environment
+ *******************************************************
+ */
+package mypackage;
+
+public class NewMyPackageClass {
+
+}
