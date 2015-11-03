64a2e360783e5f1c32764c4e9149cb91924ad79f
Dave Kirkwood <davekirkwood@hotmail.com>
2015-11-03 23:39:55 +0000

    Update NewMyPackageClass.java

--- GitTest/src/mypackage/NewMyPackageClass.java
+++ GitTest/src/mypackage/NewMyPackageClass.java
@@ -11,11 +11,4 @@ package mypackage;
  */
 public class NewMyPackageClass {
 
-/*
- External mod */
-	
-	// New mod in eclipse (3/11/15)
-	// 1) Conflict in Github.
-	// 2) Another conflict in Githbb.
-	// Change done in eclipse/firstbranch.
 }
